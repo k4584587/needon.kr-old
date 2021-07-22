7551d040339f018dfa5a420320310b3e8dfc94fc
k4584587 <k4584587@naver.com>
2021-07-22 16:48:30 +0900

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,29 +94,6 @@ class Index extends React.Component {
             <header className="major">
               <h2>Needon Server Status</h2>
             </header>
-            <ul className="statistics">
-              <li className="style1">
-                <span className="icon fa-code-fork"></span>
-                <strong>5,120</strong> Etiam
-              </li>
-              <li className="style2">
-                <span className="icon fa-folder-open-o"></span>
-                <strong>8,192</strong> Magna
-              </li>
-              <li className="style3">
-                <span className="icon fa-signal"></span>
-                <strong>2,048</strong> Tempus
-              </li>
-              <li className="style4">
-                <span className="icon fa-laptop"></span>
-                <strong>4,096</strong> Aliquam
-              </li>
-              <li className="style5">
-                <span className="icon fa-diamond"></span>
-                <strong>1,024</strong> Nullam
-              </li>
-            </ul>
-
 
           </section>
 
